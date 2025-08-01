c64c1ec0e60ff75a02f5e28741797b58bfbf978f
azadix <bartosz@madalski.pl>
2025-08-01 18:58:47 +0200

    Removed unnecessary log message

--- js/modules/RuleManager.js
+++ js/modules/RuleManager.js
@@ -132,7 +132,6 @@ export class RuleManager {
             });
 
         this.itemCodes = processedItems;
-        console.log('Processed items:', processedItems.length);
     }
 
     getRuleTypes() {
